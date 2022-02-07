e12bc8ddf97fa75ca02e651ecc3ac192a6b5dc1a
Fernando BK <fernandobenitezkleinert@gmail.com>
2022-02-07 01:32:05 -0300

    ...

--- js.js
+++ js.js
@@ -1,21 +1,38 @@
 
 async function init(){
+    // Solicitamos datos al servicio
+    let obt = await fetch('https://api.jsonbin.io/b/62008c97f77b236211eeabef');
+    if( obt.status === 200 ){
+        obt = await obt.json();
+        // Si la fecha de los datos locales es distinta a la obtenida actualizamos los datos locales.
+        if( localStorage.data_version !== Date.parse(obt.metadata.createdAt) ){
+            localStorage.data = JSON.stringify(obt.record);
+            localStorage.data_version = Date.parse(obt.metadata.createdAt);
+        }
+    }
 
-    window.d = await fetch('frases.json');
-    window.d = await d.json();
-    
+    window.data = JSON.parse(localStorage.data);
     mostrar_frase();
 }
 
-function mostrar_frase(){
-    let i = parseInt(localStorage.i) || 0;
-    if( i => window.d.length ){ i = 0; }
+function mostrar_frase(n){
+    if( n === 0 ){
+        let i = Math.floor(Math.random() * window.data.length );
+    } else {
+        let i = parseInt(localStorage.index) || 0;
+            i = i + n;
+        if( i >= window.data.length ){ i = 0; }
+    }
+    // Almacenamos nuevo índice
+    localStorage.index = i;
     
     // Imprimir frase
-    frase.innerText = window.d[i].frase;
-    autor.innerText = window.d[i].autor;
-    libro.innerText = window.d[i].libro;
-    otrainfo.innerText = window.d[i].otrainfo;
+    frase.innerText = window.data[i].frase;
+    autor.innerText = window.data[i].autor;
+    libro.innerText = window.data[i].libro;
+    otrainfo.innerText = window.data[i].otrainfo;
+}
 
-    localStorage.i = i + 1;
+function enviar_frase(){
+    prompt();
 }
