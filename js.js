ca6e5b75ea1cb3271e53a5ebc3a250e3b6a16c99
Fernando BK <fernandobenitezkleinert@gmail.com>
2022-02-08 12:54:57 -0300

    ...

--- js.js
+++ js.js
@@ -1,48 +1,25 @@
 
 async function init(){
-    // Solicitamos datos al servicio
-    let obt = await fetch('https://api.jsonbin.io/v3/b/6200bb9bf77b236211eef335', {headers: {'X-Bin-Meta':false}});
-    if( obt.status === 200 ){
-        localStorage.data = await obt.text();
-    }
-
-    window.data = JSON.parse(localStorage.data);
+    await obt_datos();
     mostrar_frase();
 }
 
-function mostrar_frase(n){
-    let i = parseInt(localStorage.index);
-    if( isNaN(i) ){ i = window.data.length - 1; }
-    
-    if( isFinite(n) ){
-        i = i + n;
-        if( i === -1 ){ i = window.data.length - 1; }
-        if( i === window.data.length ){ i = 0; }
+async function obt_datos(){
+    // Solicitamos datos al servicio
+    let obt = await fetch('https://api.jsonbin.io/v3/b/6200bb9bf77b236211eef335/latest', {headers: {'X-Bin-Meta':false}});
+    if( obt.status === 200 ){
+        localStorage.data = await obt.text();
+    } else {
+        alert('No se pudo obtener información de internet correctamente. Error '+obt.status+': '+obt.statusText);
     }
-    if( n === 0 ){
-        i = Math.floor(Math.random() * window.data.length );
-    }
-    // Almacenamos nuevo índice
-    localStorage.index = i;
-    
-    // Imprimir frase
-    frase.innerText = window.data[i].frase;
-    pie.innerText = window.data[i].pie;
-
-    return i;
 }
 
-async function enviar_frase(){
-    let p_frase = prompt('Escribir frase');
-    let p_pie = prompt('Escribir pie de la frase');
-
-    window.data.push( {frase: p_frase,pie: p_pie} );
-    
+async function enviar_datos(){
     let obt = await fetch(
         'https://api.jsonbin.io/v3/b/6200bb9bf77b236211eef335',
         {
             method: 'PUT',
-            body: JSON.stringify(window.data),
+            body: localStorage.data,
             headers: {
                 'Content-Type': 'application/json',
                 'X-Master-Key': '$2b$10$e9hka2uXqiI51ffQjR2zj.x.RW11VhFa7yJ5Ydu4x0z58ap1MKsLi', // Aclaración: Esta clave debería estar oculta o en el backend en un proyecto serio.
@@ -57,4 +34,43 @@ async function enviar_frase(){
         console.error(obt.json());
         alert(`Error ${obt.status}: ${obt.statusText} \n Ver consola.`);
     }
+}
+
+function mostrar_frase(n){
+    let data = JSON.parse(localStorage.data);
+    let i = parseInt(localStorage.index);
+    if( isNaN(i) ){ i = data.length - 1; }
+    
+    if( isFinite(n) ){
+        i = i + n;
+        if( i === -1 ){ i = data.length - 1; }
+        if( i === data.length ){ i = 0; }
+    }
+    if( n === 0 ){
+        i = Math.floor(Math.random() * data.length );
+    }
+    // Almacenamos nuevo índice
+    localStorage.index = i;
+    
+    // Imprimir frase
+    frase.innerText = data[i].frase;
+    pie.innerText = data[i].pie;
+}
+
+async function enviar_frase(){
+    let p_frase = prompt('Escribir frase: (cuidar ortografía)');
+    if( !p_frase ){ return; }
+    let p_pie = prompt('Escribir pie de la frase: (cuidar ortografía)');
+    if( !p_pie ){ return; }
+    
+    localStorage.data = JSON.stringify( JSON.parse(localStorage.data).push( {frase: p_frase,pie: p_pie} ) );
+    await enviar_datos();
+}
+
+async function eliminar_frase(){
+    let i = parseInt(localStorage.index);
+    if( confirm('Se eliminará la frase mostrada actualmente: '+window.data[i]).frase ){
+        localStorage.data = JSON.stringify( JSON.parse(localStorage.data).splice(i,1) );
+    }
+    await enviar_datos();
 }
