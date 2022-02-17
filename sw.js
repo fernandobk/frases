67b26ff170cd11388d97f484bc03eb9129c3eb29
Fernando BK <fernandobenitezkleinert@gmail.com>
2022-02-17 16:34:02 -0300

    ...

--- sw.js
+++ sw.js
@@ -1,1 +1,55 @@
 console.info('Hola. Esto es el Service Worker');
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(location.host)
+        .then(function (cache) {
+            return cache.addAll([
+                '/estructura.json',
+                '/favicon.png',
+                '/functions.js',
+                '/index.html',
+                '/manifest.json',
+                '/sw.js',
+                '/index.html',
+                '/css/fontawesome-all.min.css',
+                '/css/main.css',
+                '/css/noscript.css',
+                '/css/images/bg.jpg',
+                '/css/images/overlay-pattern.png',
+                '/css/images/overlay.svg',
+                '/css/images/ie/footer.png',
+                '/css/images/ie/footer.svg',
+                '/webfonts/fa-brands-400.eot',
+                '/webfonts/fa-brands-400.ttf',
+                '/webfonts/fa-brands-400.woff2',
+                '/webfonts/fa-regular-400.svg',
+                '/webfonts/fa-regular-400.woff',
+                '/webfonts/fa-solid-900.eot',
+                '/webfonts/fa-solid-900.ttf',
+                '/webfonts/fa-solid-900.woff2',
+                '/webfonts/fa-brands-400.svg',
+                '/webfonts/fa-brands-400.woff',
+                '/webfonts/fa-regular-400.eot',
+                '/webfonts/fa-regular-400.ttf',
+                '/webfonts/fa-regular-400.woff2',
+                '/webfonts/fa-solid-900.svg',
+                '/webfonts/fa-solid-900.woff'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
